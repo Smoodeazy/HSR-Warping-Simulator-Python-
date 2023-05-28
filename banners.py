823f44884b2b1665b5af102a5da73d7f32582ded
Kyotsu <103213491+Smoodeazy@users.noreply.github.com>
2023-05-29 00:07:05 +0100

    Create banners.py

---
+++ banners.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+fiveStars: list = ["Seele", "Jing Yuan"]
+
+light3Cones: list = ["Adversarial", "Amber", "Arrows", "Chorus", "Collapsing Sky", "Cornucopia", "Darting Arrow", "Data Bank", "Defense",
+                    "Fine Fruit", "Hidden Shadow", "Loop", "Mediation", "Meshing Cogs", "Multiplication", "Mutual Demise", "Passkey", "Pioneering", "Sagacity",
+                    "Shattered Home", "Void"]
+
+light4Cones: list = ["A Secret Vow", "Carve the Moon, Weave the Clouds", "Dance! Dance! Dance!", "Day One of My New Life", "Eyes of the Prey", "Fermata", "Geniuses' Repose",
+                     "Good Night and Sleep Well", "Landau's Choice", "Make the World Clamor", "Memories of the Past", "Nowhere to Run", "Only Silence Remains",
+                     "Past and Future", "Perfect Timing", "Planetary Rendezvous", "Post-Op Conversation", "Quid Pro Quo", "Resolution Shines As Pearls of Sweat",
+                     "Return to Darkness", "River Flows in Spring", "Shared Feeling", "Subscribe for More!", "Swordplay", "The Birth of the Self",
+                     "The Moles Welcome You", "The Seriousness of Breakfast", "This Is Me!", "Today Is Another Peaceful Day", "Trend of the Universal Market", "Under the Blue Sky",
+                     "Warmth Shortens Cold Nights", "We Are Wildfire", "We Will Meet Again", "Woof! Walk Time!"]
+
+"""
+Standard banner stuff, default items inside banners
+"""
+standard5Star: list = ["Bailu", "Bronya", "Clara", "Gepard", "Himeko", "Welt", "Yanqing"]
+standard4Star: list = ["Arlan", "Asta", "Dan Heng", "Herta", "Hook", "March 7th", "Natasha", "Pela", "Qingque", "Sampo", "Serval", "Sushang", "Tingyun"]
+
+standard3Cones: list = ["Passkey", "Meshing Cogs", "Loop", "Defense", "Shattered Home", "Fine Fruit", "Darting Arrow", "Data Bank", "Chorus", "Void", "Amber",
+                        "Collapsing Sky", "Cornucopia", "Arrows"]
+standard4Cones: list = ["Make the World Clamor", "A Secret Vow", "Planetary Rendezvous", "Swordplay", "Landau's Choice", "Eyes of the Prey", "Shared Feeling",
+                        "The Birth of the Self", "The Moles Welcome You", "Memories of the Past", "Only Silence Remains", "Day One of My New Life",
+                        "Good Night and Sleep Well", "Post-Op Conversation"]
+
+"""
+Current Event Banners at the moment
+"""
+
+eventBanners: dict = {"Seele" : ["Hook", "Natasha", "Pela"], "Jing Yuan": ["March 7th", "Sushang", "Tingyun"]}
